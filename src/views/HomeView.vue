356df2ef2149cf768af53974e7682b656df16acc
memorah <filip.mackov@gmail.com>
2022-10-17 12:36:19 +0200

    counter methods and data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,29 @@
   <div class="home">
     <h1>HOME</h1>
     <div>
-      <button class="btn">-</button>
-      <span class="counter">0</span>
-      <button class="btn">+</button>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    },
+  },
+};
+</script>
 <style>
 .home {
   text-align: center;
