d6cae853b168baa2a89663d78f96a19a3053748f
memorah <filip.mackov@gmail.com>
2022-10-20 15:10:05 +0200

    router use router

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -3,15 +3,20 @@
     <h1>Posts</h1>
     <p>Display the conten of post with ID of {{ $route.params.id }} here!</p>
     <div><button @click="showPostId">Show post ID</button></div>
-    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
+  <div><button @click="goHomeIn3Seconds">Go Home in 3s</button></div>
+  <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
 </template>
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 
 const showPostId = () => {
   alert(`the ID of this post is: ${route.params.id}`);
+};
+const goHomeIn3Seconds = () => {
+  setTimeout(() => router.push({ name: "home" }), 3000);
 };
 </script>
