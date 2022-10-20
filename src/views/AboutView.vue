99f08af7b80fac96c14ef03ab895bb42f3330a69
memorah <filip.mackov@gmail.com>
2022-10-20 14:34:45 +0200

    router

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>ABOUT</h1>
-    <textarea v-autofocus></textarea>
-  </div>
-</template>
-<script setup>
-import { vAutofocus } from "@/directives/vAutofocus";
-</script>
---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="post-detail">
+    <h1>Posts</h1>
+    <p>Display the conten of post with ID of {{ $route.params.id }} here!</p>
+    <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
+  </div>
+</template>
+<script setup></script>
