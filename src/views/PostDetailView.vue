ae26f104a6cf58ed4ee6fe717fd9a747ae032e68
memorah <filip.mackov@gmail.com>
2022-10-20 15:00:01 +0200

    route alert

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -2,7 +2,16 @@
   <div class="post-detail">
     <h1>Posts</h1>
     <p>Display the conten of post with ID of {{ $route.params.id }} here!</p>
+    <div><button @click="showPostId">Show post ID</button></div>
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const showPostId = () => {
+  alert(`the ID of this post is: ${route.params.id}`);
+};
+</script>
