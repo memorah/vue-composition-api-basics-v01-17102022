31a2eca41ebe5bc46c90b856c6556e5b36b92292
memorah <filip.mackov@gmail.com>
2022-10-21 13:57:59 +0200

    edit

---
+++ src/views/ModalsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button>Show modal</button>
+  </div>
+  <div class="modal">
+    <h1>This is modal</h1>
+    <p>Lorem ipsum do lore</p>
+    <button>Hide modal</button>
+  </div>
+</template>
+
+<script setup>
+/* modals */
+</script>
