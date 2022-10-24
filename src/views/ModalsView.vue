5016819b337a1fd1dec97e72a3d672f6a983dec3
memorah <filip.mackov@gmail.com>
2022-10-24 09:27:38 +0200

    showModal modal

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="modals">
     <h1>Modals</h1>
-    <button>Show modal</button>
+    <button @click="showModal = true">Show modal</button>
   </div>
-  <div class="modal">
+  <div v-if="showModal" class="modal">
     <h1>This is modal</h1>
     <p>Lorem ipsum do lore</p>
-    <button>Hide modal</button>
+    <button @click="showModal = false">Hide modal</button>
   </div>
 </template>
 
 <script setup>
+/*imports*/
+import { ref } from "vue";
 /* modals */
+const showModal = ref(false);
 </script>
