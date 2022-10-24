cee117be07585bdb9a3f93c9ec83a78d49ab53c9
memorah <filip.mackov@gmail.com>
2022-10-24 09:33:39 +0200

    style modal

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -16,3 +16,17 @@ import { ref } from "vue";
 /* modals */
 const showModal = ref(false);
 </script>
+<style>
+.modal {
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background: #00bd7e;
+  border-radius: 5px;
+  color: white;
+}
+</style>
